e9a5b7900dcbeb77ffb9366ed8651b96266d1f9b
Nataly <naesdu@gmail.com>
2022-02-05 18:05:45 -0500

    responsive otras pantallas

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,58 +1,50 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { connect } from "react-redux";
-import styles from "./Product.module.css";
-import Navbar from "../../components/Navbar/Navbar";
-import Footer from "../../components/Footer/Footer";
-import Announcement from "../../components/Announcement/Announcement";
+
 import Products from "../../components/Products/Products1/Products";
 import { addToCart } from "../../redux/Shopping/shopping-actions";
 
+import styles from "./Product.module.css";
+import Announcement from "../../components/Announcement/Announcement";
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
+import Newsletter from "../../components/Newsletter/ContactForm";
+
 const Product = ({ current, addToCart }) => {
   return (
-    <>
-    <Announcement/>
-    <Navbar />
     <div className={styles.Container}>
+      <Announcement />
+      <Navbar />
       
-
       <div className={styles.Wrapper}>
         <div className={styles.ImgContainer}>
           <img className={styles.Image} src={current.img} alt={current.title} />
         </div>
         <div className={styles.InfoContainer}>
-      
           <h1 className={styles.Title}>{current.title}</h1>
           <p className={styles.Desc}>{current.description}</p>
-          <span className={styles.Price}>${current.price}</span>
-          
+          <span className={styles.Price}>$ {current.price}</span>
           <div className={styles.FilterContainer}>
             <div className={styles.Filter}>
-              <span className={styles.FilterTitle}>Medidas: </span>
-
-              {current.measure}
+              <span className={styles.FilterTitle}>
+                Medidas: {current.measure}
+              </span>
             </div>
           </div>
           <div className={styles.AddContainer}>
-            {/* <div className={styles.AmountContainer}>
-              <Remove />
-              <span className={styles.Amount}>{current.qty}</span>
-              <Add />
-            </div> */}
-
             <button
-              className={styles.Button}
               onClick={() => addToCart(current.id, current.category)}
+              className={styles.Button}
             >
-              AÑADIR AL CARRITO
+              AÑADIR
             </button>
           </div>
         </div>
       </div>
-
+      <Newsletter />
       <Products />
-
       <Footer />
     </div>
-    </>
   );
 };
 
@@ -69,3 +61,63 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Product);
+
+//
+
+// const Product = ({ current, addToCart }) => {
+//   return (
+//     <>
+//     <Announcement/>
+//     <Navbar />
+//     <div className={styles.Container}>
+
+//       <div className={styles.Wrapper}>
+//         <div className={styles.ImgContainer}>
+//           <img className={styles.Image} src={current.img} alt={current.title} />
+//         </div>
+//         <div className={styles.InfoContainer}>
+
+//           <h1 className={styles.Title}>{current.title}</h1>
+//           <p className={styles.Desc}>{current.description}</p>
+//           <span className={styles.Price}>${current.price}</span>
+
+//           <div className={styles.FilterContainer}>
+//             <div className={styles.Filter}>
+//               <span className={styles.FilterTitle}>Medidas: </span>
+
+//               {current.measure}
+//             </div>
+//           </div>
+//           <div className={styles.AddContainer}>
+
+//             <button
+//               className={styles.Button}
+//               onClick={() => addToCart(current.id, current.category)}
+//             >
+//               AÑADIR AL CARRITO
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+
+//       <Products />
+
+//       <Footer />
+//     </div>
+//     </>
+//   );
+// };
+
+// const mapStateToProps = (state) => {
+//   return {
+//     current: state.shop.currentItem,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addToCart: (id, category) => dispatch(addToCart(id, category)),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Product);
